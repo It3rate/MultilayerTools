25d72c0f18727bd0baff95eeb0087096debe94ad
Robin Debreuil <debreuil.robin@gmail.com>
2021-03-17 17:28:37 -0500

    Got CreateShelves working with new command system.

--- MutliLayerTools.py
+++ MutliLayerTools.py
@@ -3,12 +3,14 @@ import adsk.core, adsk.fusion, traceback, os
 from .CopySketch import CopySketchCommand
 from .PasteSketch import PasteSketchCommand
 from .ExtrudeLayers import ExtrudeLayersCommand
+from .lib.CreateShelves import CreateShelves
 
 commandHandles = []
 def run(context):
     commandHandles.append(ExtrudeLayersCommand())
     commandHandles.append(CopySketchCommand())
     commandHandles.append(PasteSketchCommand())
+    # commandHandles.append(CreateShelves())
 
 def stop(context):
     for cmd in commandHandles:
